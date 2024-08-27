7d61ed8018b2d23c1a3fb2925eddb732689945a5
EkanshBatra <>
2024-08-27 12:26:39 +0530

    initial commit

---
+++ Assets/Scripts/Managers/UIManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UIManager : MonoBehaviour
+{
+    [Header("Buttons")]
+    [SerializeField]
+    private Button Spin_Button;
+    [SerializeField]
+    private Button AutoSpin_Button;
+    [SerializeField]
+    private Button Menu_Button;
+}
